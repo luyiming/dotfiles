d1b3a397dce3701ebe29b63d0474205b90dcf9df
luyiming <luyimingchn@gmail.com>
2026-09-01 22:39:03 +0800

    feat(fish): enhance git command abbreviations for improved workflow

--- fish/conf.d/50-git.fish
+++ fish/conf.d/50-git.fish
@@ -1,5 +1,35 @@
-abbr -a gst "git status"
+# Status
+abbr -a gst 'git status'
+abbr -a gsb 'git status --short --branch'
 
+# Add
+abbr -a ga 'git add'
+abbr -a gaa 'git add --all'
+abbr -a gapa 'git add --patch'
+
+# Diff
+abbr -a gd 'git diff'
+abbr -a gds 'git diff --staged'
+abbr -a gdw 'git diff --word-diff'
+
+# Commit
+abbr -a gc 'git commit --verbose'
+
+# History
+abbr -a gsh 'git show'
+abbr -a glo 'git log --oneline --decorate'
+abbr -a glog 'git log --oneline --decorate --graph'
+abbr -a gloga 'git log --oneline --decorate --graph --all'
+
+# Branching
 abbr -a gco "git checkout"
+abbr -a gsw 'git switch'
+abbr -a gswc 'git switch -c'
 
-abbr -a gcm "git commit"
+# Remote
+abbr -a gf 'git fetch'
+abbr -a gl 'git pull'
+abbr -a gp 'git push'
+
+# Worktree
+abbr -a gwt 'git worktree'
